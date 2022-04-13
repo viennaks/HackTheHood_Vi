a1e39b247598587cb19816c1303f0aff45110622
Vi Le <53014202+viennaks@users.noreply.github.com>
2022-04-13 14:37:35 -0700

    Create Python_Variables.py

---
+++ Python_Variables.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+name = "VI"
+age = 25
+years_at_hth = 1.5
+plays_basketball = True
+jersey_number = "13"
+
+car_name = "Honda"
+x = 50
+my_first_name = "Vi"
+
+print(name, age, years_at_hth, plays_basketball, jersey_number, car_name, x, my_first_name)
