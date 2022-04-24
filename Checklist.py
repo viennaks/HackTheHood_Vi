319f8d19a981f3497eb1b10dd94582472b8ac761
Vi Le <53014202+viennaks@users.noreply.github.com>
2022-04-24 00:29:31 -0700

    Terminal Checklist

---
+++ Checklist.py
@@ -0,0 +1,63 @@
+from tabnanny import check
+
+checklist = [ 1, 2, 1, 1, 1, 1,1 ,1 ]
+
+# Define Functions
+def create(item):
+    checklist.append(item)
+
+def read(index):
+    print(checklist[index])
+
+def update(index, item):
+    checklist[index]= item
+
+def destroy(index):
+    checklist.pop(index)
+
+def mark_completed(index):
+    checklist[index] += "*"
+
+def list_all_items():
+    print(checklist)
+
+def user_input(prompt):
+    x = input(prompt)
+    return x
+    
+def select(function_code):  
+
+    function_code = user_input("What functions do you want to run? Create (c), Read (r), Update (u), Destroy (d), Mark Complete (m), and List all Items (l)\n Input a letter: ")
+
+    if function_code == "c":
+        item = user_input("Input item: ")
+        create(item)
+
+    elif function_code == "r":
+        index = user_input("Input index: ")
+        read(index)
+
+    elif function_code == "u":
+        index = user_input("Input index: ")
+        item = user_input("Input item: ")
+        update(index, item)
+
+    elif function_code == "d":
+        index = user_input("Input index: ")
+        destroy(index)
+
+    elif function_code == "m":
+        index = user_input("Input index: ")
+        mark_completed(index)
+
+    elif function_code == "l":
+        list_all_items()
+
+running = True
+
+while running:
+    selection = user_input(
+        "Press any key to start")
+    running = select(selection)
+ 
+ 
